ac378d42f6fe10beb28c5983bd4fc68b7d00e2ba
prestosilver <prestosilver.ptp@gmail.com>
2026-07-27 10:28:19 -0400

    Add doc comments to funcs.lua and gaps.lua

--- libs/funcs.lua
+++ libs/funcs.lua
@@ -4,10 +4,22 @@ local function pt(pixels)
     return pixels * 1.3333
 end
 
+--- Set the font size in points.
+--- @param name string The name of the font to use.
+--- @param size number The size (in points) to use
 function M.set_font_pt(name, size)
     session:set_font(name, pt(size))
 end
 
+--- Set the font size in pixels.
+--- @param name string The name of the font to use.
+--- @param size number The size (in pixels) to use
+function M.set_font_pixels(name, size)
+    session:set_font(name, size)
+end
+
+--- Creates a callback that reloads Conpositor
+--- @return fun() # Returns a function callback
 function M.reload()
     return function()
         package.loaded["init"] = nil
@@ -15,6 +27,9 @@ function M.reload()
     end
 end
 
+--- Creates a callback that sets the active client border to size
+--- @param size number The size (in pixels) of the client border
+--- @return fun() # Returns a function that sets the client border to size
 function M.set_client_border(size)
     local size = size
     return function()
@@ -25,6 +40,8 @@ function M.set_client_border(size)
     end
 end
 
+--- Creates a callback that toggles the floating state of the active client
+--- @return fun() # Returns a function callback
 function M.toggle_floating()
     return function()
         local client = session:active_client()
@@ -34,6 +51,8 @@ function M.toggle_floating()
     end
 end
 
+--- Creates a callback that toggles the fullscreen state of the active client
+--- @return fun() # Returns a function callback
 function M.toggle_fullscreen()
     return function()
         local client = session:active_client()
@@ -43,6 +62,8 @@ function M.toggle_fullscreen()
     end
 end
 
+--- Creates a callback that kills the active client
+--- @return fun() # Returns a function callback
 function M.kill_client()
     return function()
         local client = session:active_client()
@@ -52,6 +73,9 @@ function M.kill_client()
     end
 end
 
+--- Creates a callback that sets the tag of the active monitor
+--- @param tag number The tag to set
+--- @return fun() # Returns a function callback
 function M.set_monitor_tag(tag)
     local tag = tag
     return function()
@@ -62,6 +86,9 @@ function M.set_monitor_tag(tag)
     end
 end
 
+--- Creates a callback that sets the tag of the active client
+--- @param tag number The tag to set
+--- @return fun() # Returns a function callback
 function M.set_client_tag(tag)
     local tag = tag
     return function()
@@ -72,6 +99,9 @@ function M.set_client_tag(tag)
     end
 end
 
+--- Creates a callback that sets the stack of the active client
+--- @param stack number The stack to set
+--- @return fun() # Returns a function callback
 function M.set_client_stack(stack)
     local stack = stack
     return function()
@@ -108,13 +138,21 @@ function M.cycle_layout(direction, lists)
     end
 end
 
+--- Creates a callback that cycles the focus in the active stack
+--- @param stack number The direction to cycle -1 is backwards 1 is forwards
+--- @return fun() # Returns a function callback
 function M.cycle_focus(direction)
+    -- TODO: make direction a string
     local direction = direction
     return function()
         session:cycle_focus(direction)
     end
 end
 
+--- Creates a callback that spawns a command
+--- @param program string The program to call
+--- @param args Any arguments to pass
+--- @return fun() # Returns a function callback
 function M.spawn(program, args)
     local program = program
     local args = args
@@ -123,6 +161,8 @@ function M.spawn(program, args)
     end
 end
 
+--- Creates a callback that quits conpositor
+--- @return fun() # Returns a function callback
 function M.quit()
     return function()
         session:quit()
--- libs/gaps.lua
+++ libs/gaps.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+-- TODO: Make this per monitor
+
 local function update_gaps()
     local set_gaps_value = 0
     local set_gaps_valuei = 0
@@ -15,16 +17,21 @@ local function update_gaps()
     end
 end
 
+--- Toggles gaps on the active monitor
 function M.toggle()
     M.gaps_toggle = not M.gaps_toggle
 
     update_gaps()
 end
 
+--- Gets the current gaps radius on the active monitor
+--- @return number The size of the gaps
 function M.get()
     return M.gaps_value
 end
 
+--- Sets the current gaps radius on the active monitor
+--- @param value number The size of the gaps
 function M.set(value)
     M.gaps_value = value
     if M.gaps_value < 0 then
@@ -34,14 +41,25 @@ function M.set(value)
     update_gaps()
 end
 
+--- Increases the current gaps radius on the active monitor
 function M.increase()
     M.set(M.gaps_value + M.gaps_inc)
 end
 
+--- Increases the current gaps radius on the active monitor
 function M.decrease()
     M.set(M.gaps_value - M.gaps_inc)
 end
 
+---@class GapsConfig
+---@field inc ?string The size to increase and decrease gaps by
+---@field value ?bool The default visibility of gaps
+---@field toggle ?number The default gaps radius
+---@field ratio ?number How many pixels increase the outer radius
+---@field outer ?number How many pixels to add to the outer radius
+
+--- Init gaps
+--- @param config GapsConfig The module config
 function M.setup(config)
     if M.init then
         return
